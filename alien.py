7479b88e6498c8de91bac82112ecad8dd74763b2
wh_ly <klywh100@163.com>
2019-03-06 10:55:41 +0800

    change alien

--- alien.py
+++ alien.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """表示单个外星人的类"""
     def __init__(self,ai_settings,screen):
-        """初始化外星人并设置其初始位置"""
+        """初始化外星人,并且设置其初始位置"""
         super(Alien,self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
